84662e9076489b7e163c96abfe1a899151dd3e21
a.johnston <a.johnston@bc3139a8-67e5-0310-9ffc-ced21a209358>
2016-09-01 19:06:39 +0000

    Fixed an issue with the correlation matrix such that only significant p values are considered (thus increasing the intercorrelations)
    Added plotting layouts
    
    Fixed issue with Bionet wrapper
    
    From: Andrew Johnston <Andrew.Johnston@med.einstein.yu.edu>
    
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/SMITE@120656 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/SMITE.R
+++ R/SMITE.R
@@ -300,25 +300,25 @@ setMethod(
         {
             stop("Provided data set is already loaded as mod_type")
         }
-
+        
         unique_feature_names <- unique(
             unlist(slot(pvalue_annotation, "annotation"))$feature)
-
+        
         ##no weights provided
         if(missing(weight_by)){
             weight_by <- rep("pval", length(unique_feature_names[!unique_feature_names %in%
-                            c("original", "tss")]))
+                                                                     c("original", "tss")]))
         }
-
+        
         ##no mod_included or weight names
         if(is.null(names(weight_by))){
             if(is.null(mod_included)){
                 mod_included <- unique_feature_names[!unique_feature_names %in%
-                            c("original", "tss")]
+                                                         c("original", "tss")]
             }
             names(weight_by) <- mod_included
         }
-
+        
         ##weight names were provided
         if(!is.null(names(weight_by))){
             mod_included <- names(weight_by)
@@ -326,24 +326,24 @@ setMethod(
                 stop("Provided weight names must match those in
                      unique(GenomicRanges::mcols(unlist(pvalue_annotation@annotation))$feature)")
             }
-        }
-
+            }
+        
         if(any(!c(-1, 1) %in% unique(sign(mod_data[, 4])))){
             message("WARNING: Effects should provide a direction,
-                but these effects are all in the same direction.")
+                    but these effects are all in the same direction.")
         }
-
+        
         if(any(mod_data[, 5] < 0,mod_data[, 5] > 1)){
             stop("P-values must be between 0 and 1")
         }
-
-
+        
+        
         mod_grange <- GenomicRanges::GRanges(seqnames=mod_data[, 1],
-            ranges=IRanges::IRanges(start=mod_data[, 2], end=mod_data[, 3]),
-            effect=mod_data[, 4], pval=mod_data[, 5], type=mod_type)
-
+                                             ranges=IRanges::IRanges(start=mod_data[, 2], end=mod_data[, 3]),
+                                             effect=mod_data[, 4], pval=mod_data[, 5], type=mod_type)
+        
         temp_annotation <- unlist(slot(pvalue_annotation, "annotation"))
-
+        
         overlap_mods <- GenomicRanges::findOverlaps(temp_annotation, mod_grange)
         mod_grange_overlaps <- mod_grange[S4Vectors::subjectHits(overlap_mods)]
         GenomicRanges::mcols(mod_grange_overlaps) <- cbind(GenomicRanges::mcols(
@@ -352,8 +352,8 @@ setMethod(
             GenomicRanges::mcols(mod_grange_overlaps))
         mod_grange_overlaps <- split(mod_grange_overlaps, mod_grange_overlaps$name)
         temp_annotation <- split(temp_annotation, temp_annotation$name)
-
-
+        
+        
         if(mod_corr == TRUE){
             if(verbose == TRUE){
                 message("Computing correlation matrices")
@@ -361,15 +361,15 @@ setMethod(
             temp_split_mod_grange <- split(mod_grange, GenomicRanges::seqnames(mod_grange))
             precede_follow_each_element <- lapply(temp_split_mod_grange,
                                                   function(chr){
-                temp_chr <- IRanges(start(chr), end(chr))
-                temp_precede <- precede(temp_chr)
-                temp_follow <- follow(temp_chr)
-                temp_precede[which(is.na(temp_precede))] <-
-                    which(is.na(temp_precede))
-                temp_follow[which(is.na(temp_follow))] <-
-                    which(is.na(temp_follow))
-                chr[c(temp_follow, temp_precede)]
-            })
+                                                      temp_chr <- IRanges(start(chr), end(chr))
+                                                      temp_precede <- precede(temp_chr)
+                                                      temp_follow <- follow(temp_chr)
+                                                      temp_precede[which(is.na(temp_precede))] <-
+                                                          which(is.na(temp_precede))
+                                                      temp_follow[which(is.na(temp_follow))] <-
+                                                          which(is.na(temp_follow))
+                                                      chr[c(temp_follow, temp_precede)]
+                                                  })
             mod_grange_corr <- unlist(GRangesList(precede_follow_each_element))
             
             duplicate_each_chr <- lapply(temp_split_mod_grange, function(chr){
@@ -380,14 +380,14 @@ setMethod(
                                                           mod_grange_corr)
             
             mod_grange_corr$pval2 <- duplicate_each_chr$pval
-            
-            quantile_distances_mod_corr <- Hmisc::cut2(mod_grange_corr$distance, 
-                g=500, onlycuts=TRUE)
+            mod_grange_corr <- mod_grange_corr[which(mod_grange_corr$pval2<0.05)]
+            quantile_distances_mod_corr <- Hmisc::cut2(mod_grange_corr$distance,
+                                                       g=500, onlycuts=TRUE)
             quantile_distances_mod_corr[length(quantile_distances_mod_corr)] <- 250000000
             mod_grange_corr$cat <- cut(mod_grange_corr$distance, breaks=quantile_distances_mod_corr)
             mod_grange_corr <- split(mod_grange_corr, mod_grange_corr$cat)
             mod_grange_corr2 <- lapply(mod_grange_corr, function(j){
-                mean((sapply(1:100, function(i){
+                mean((sapply(1:500, function(i){
                     index <- sample(1:length(j), replace=TRUE);
                     cor(qnorm(1-j$pval[index]), qnorm(1-j$pval2[index]))
                 })))
@@ -397,7 +397,7 @@ setMethod(
                                      as.character(names(mod_grange_corr2)),
                                      stringsAsFactors=FALSE)
             final_corr <- rbind(c(.9, paste("(-1, ", quantile_distances_mod_corr[1], "]",
-                                          sep="")), final_corr)
+                                            sep="")), final_corr)
             rm(mod_grange_corr)
         }
         combined_pvalues_list <- sapply(mod_included, function(i){
@@ -405,15 +405,15 @@ setMethod(
                 message(paste("Combining p-values over:", i))
             }
             temp <- subset(unlist(mod_grange_overlaps), unlist(mod_grange_overlaps)$feature == i)
-
-
+            
+            
             ref_data <- unlist(slot(pvalue_annotation, "annotation"))
             ref_data <-
                 subset(ref_data,
                        ref_data$feature == "tss")
             ref_data <- ref_data[temp$name]
             suppressWarnings(temp$distance <- distance(ref_data,
-                                                            temp)+2)
+                                                       temp)+2)
             temp <- split(temp, temp$name)
             forreturn <- lapply(temp, function(each){
                 each_length <- length(each)
@@ -427,37 +427,38 @@ setMethod(
                             breaks=c(-1, quantile_distances_mod_corr)), final_corr[, 2]), 1]),
                             ncol=each_length)
                         diag(corr_mat) <- 1
+                        corr_mat<-abs(corr_mat)
                         chol_d <- try(chol(corr_mat), silent=TRUE)
-                        if(is(chol_d, "try-error"))
+                        while(is(chol_d, "try-error"))
                         {
                             index<- as.numeric(strsplit(
                                 strsplit(chol_d[1],
                                          "the leading minor of order ")[[1]][2],
                                 " is not positive")[[1]][1])-1
-                            chol_d <- chol(corr_mat[1:index, 1:index])
+                            chol_d <- try(chol(corr_mat[1:index, 1:index]),silent=TRUE)
                             each_pval <- each_pval[1:index]
                             each_effect <- each_effect[1:index]
                             distances <- distances[1:index]
                             each_length <- index
                         }
-
-                        each_pval <- 1-pnorm(abs(solve(chol_d) %*% qnorm(
+                        
+                        each_pval <- 1-pnorm(abs(solve(t(chol_d)) %*% qnorm(
                             1-each_pval/2)))
                         each_pval<-replace(each_pval, each_pval == 0 , 0.000000001)
                         each_pval <- each_pval*2
                     }
-
+                    
                     if(weight_by_method == "Stouffer"){
-
+                        
                         if(weight_by[i] == "distance"){
                             ##mean is weighted by distance
                             out_mean <- weighted.mean(each_effect,
-                                                    w=(1/log(distances)))
+                                                      w=(1/log(distances)))
                             ##Stouffer test is weighted by distance
                             out_pval <- stoufferTest(each_pval, weights=(1/log(distances)))
                         }
                         else if(weight_by[i] %in%
-                                      c("pval", "p.value", "pvalue", "p_val")){
+                                c("pval", "p.value", "pvalue", "p_val")){
                             ##mean is weight by pvalue
                             out_mean <- weighted.mean(each_effect, w=-log(each_pval))
                             out_pval <- stoufferTest(each_pval, weights=NULL)
@@ -467,10 +468,10 @@ setMethod(
                             out_mean <- mean(each_effect, na.rm=TRUE)
                             out_pval <- stoufferTest(each_pval, weights=NULL)
                         }
-
+                        
                     }
                     else if(weight_by_method %in% c("minimum", "Sidak", "sidak")){
-
+                        
                         index <- which(each_pval == min(each_pval))
                         if(length(index) > 1){
                             index <- index[which(
@@ -478,10 +479,10 @@ setMethod(
                         }
                         out_mean <- each_effect[index]
                         out_pval <- 1-(1-each_pval[index])^length(each_pval)
-
+                        
                     }
                     else if(weight_by_method == "binomial"){
-
+                        
                         index <- which(each_pval == min(each_pval))
                         if(length(index) > 1){
                             index <- index[which(abs(each_effect[index]) == max(
@@ -489,12 +490,12 @@ setMethod(
                         }
                         out_mean <- each_effect[index]
                         out_pval <- (1-pbinom(q=length(which(each_pval<0.05)),
-                                            size=each_length, prob=0.05))
+                                              size=each_length, prob=0.05))
                     } else if(weight_by_method %in%
-                                  c("Fisher", "fisher", "chisq", "chi")){
+                              c("Fisher", "fisher", "chisq", "chi")){
                         out_pval <- 1-pchisq(-2*sum(log(each_pval)), each_length*2)
                         out_mean <- mean(sign(each_effect), na.rm=TRUE)
-
+                        
                     }
                 }
                 #if only one pval
@@ -502,13 +503,13 @@ setMethod(
                     out_mean <- each_effect
                     out_pval <- each_pval
                 }
-
+                
                 c(out_mean, out_pval, each_length)
             })
-
+            
             do.call(rbind, forreturn)
         })
-
+        
         if(verbose == TRUE){
             message("Quantile permuting scores")
         }
@@ -516,15 +517,15 @@ setMethod(
             categories <- data.frame(categories=as.numeric(
                 Hmisc::cut2(each_feature[, 3], g=100)))
             categories_table <- data.frame(table(categories))
-
+            
             trans_p <- cbind(trans=qnorm(1-each_feature[, 2]/2),
-                           join(categories, categories_table, by="categories"))
-
+                             plyr::join(categories, categories_table, by="categories"))
+            
             trans_p[, 1] <- replace(trans_p[, 1],is.infinite(trans_p[, 1]),
                                     max(subset(trans_p, !is.infinite(
                                         trans_p[, 1])), na.rm=TRUE))
-
-
+            
+            
             num_list <- split(trans_p$trans, trans_p$categories)
             rand_list <- sapply(1:length(num_list), function(i){
                 as.matrix(sapply(1:500, function(j){
@@ -539,37 +540,37 @@ setMethod(
             new_pval <- replace(new_pval, new_pval == 0,
                                 min(subset(new_pval, new_pval != 0),
                                     na.rm=TRUE))
-
+            
             each_feature[, 2] <- new_pval
             each_feature <- as.data.frame(each_feature)
             each_feature
         })
-
+        
         if(verbose == TRUE){
             message("Scores have been adjusted")
         }
-
-        newmods <- c(unlist(slot(pvalue_annotation, "modifications")), 
-            unlist(mod_grange_overlaps))
+        
+        newmods <- c(unlist(slot(pvalue_annotation, "modifications")),
+                     unlist(mod_grange_overlaps))
         names(newmods) <- NULL
         newmods <- split(newmods, newmods$name)
-
+        
         output_m_summary <-
             suppressWarnings(as.data.frame(c(list(names=names(mod_grange_overlaps)),
                                              lapply(combined_pvalues_list, function(x){
-                                                    x[match(names(
-                                                        mod_grange_overlaps),
-                                                        rownames(x)), 1:2]
-                                                    }))))
+                                                 x[match(names(
+                                                     mod_grange_overlaps),
+                                                     rownames(x)), 1:2]
+                                             }))))
         rownames(output_m_summary) <- output_m_summary[, 1]
         output_m_summary <- output_m_summary[, -1]
         colnames(output_m_summary) <- paste(mod_type,
-                                 apply(expand.grid(c("effect", "pvalue"),
-                                                   mod_included),1, function(i){
-                                                        paste(i[2], i[1],
-                                                              sep="_")
-                                                    }), sep="_")
-
+                                            apply(expand.grid(c("effect", "pvalue"),
+                                                              mod_included),1, function(i){
+                                                                  paste(i[2], i[1],
+                                                                        sep="_")
+                                                              }), sep="_")
+        
         newmetadata <- slot(slot(pvalue_annotation, "modifications"), "metadata")
         if(is.null(newmetadata$m_summary)){
             newmetadata$m_summary <- output_m_summary
@@ -584,11 +585,11 @@ setMethod(
         newmetadata[["elements"]][[mod_type]]$weight_by_method <- weight_by_method
         newmetadata$elementnames <- c(newmetadata$elementnames,
                                       paste(mod_type, mod_included, sep="_"))
-
+        
         slot(newmods, "metadata") <- newmetadata
         slot(pvalue_annotation, "modifications") <- newmods
         pvalue_annotation
-    }
+        }
 )
 
 setMethod(
@@ -1130,38 +1131,38 @@ setMethod(
             slot(slot(pvalue_annotation,"score_data"),"module_output") <- list()
             message("Overwriting existing modules.")
         }
-
+        
         scores <- highScores(pvalue_annotation, alpha=alpha)
         pval.v <- exp(scores/(-2))
-        g <- subNetwork(names(pval.v), network)
+        g <- suppressWarnings(subNetwork(names(pval.v), network))
         g <- rmSelfLoops(g)
         if(inherits(g,what="igraph")){
             g <- as_graphnel(g)
         }
-
+        
         heinzOut <- suppressWarnings(runFastHeinz(g, scores))
-
+        
         output <- list();
         output[[1]] <- list(heinzOut@nodes);
         output[[2]] <- pval.v;
         output[[3]] <- scores;
         output[[4]] <- g;
         output[[6]] <- "BioNet";
-
+        
         names(output) <- c("modules",  "p_value",
-                          "statistic","network","moduleStats","moduleType");
+                           "statistic","network","moduleStats","moduleType");
         sub_score <- subset(scores,names(scores)%in%output[[1]][[1]])
         stat.mod <- sum(abs(sub_score))
         pval.mod <- 1-pchisq(stat.mod,2*length(output[[1]][[1]]))
         output[[5]] <-
             list(c(statMod=stat.mod, pvalMod=pval.mod,
                    lenMod=length(output[[1]][[1]]))
-                 )
-
-
+            )
+        
+        
         names(output[[1]])[1] <-
             names(sub_score[which(sub_score==max(sub_score))[1]]
-                  )
+            )
         slot(slot(pvalue_annotation,"score_data"),"module_output") <- output
         pvalue_annotation
     }
