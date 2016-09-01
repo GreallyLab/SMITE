89c936f30c7506741207b7b286dc238854a8b343
a.johnston <a.johnston@bc3139a8-67e5-0310-9ffc-ced21a209358>
2016-09-01 19:06:39 +0000

    Fixed an issue with the correlation matrix such that only significant p values are considered (thus increasing the intercorrelations)
    Added plotting layouts
    
    Fixed issue with Bionet wrapper
    
    From: Andrew Johnston <Andrew.Johnston@med.einstein.yu.edu>
    
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/SMITE@120656 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/SMITE-plot.R
+++ R/SMITE-plot.R
@@ -60,9 +60,9 @@ setMethod(
                         exp_mid_col="gray90", label_scale=TRUE,
                         compare_plot=FALSE, pdf_out=NULL)
     {
-
+        
         module_output <- slot(slot(pvalue_annotation, "score_data"), "module_output")
-
+        
         if(!is.null(pdf_out)){
             unlink(pdf_out)
             if(compare_plot==TRUE){
@@ -115,7 +115,7 @@ setMethod(
         for(n_plot in which_network){ # This is the big loop, with n_plot being the chosen GoSeq networks
             if(goseq == TRUE){
                 if(length(module_output$goseqOut) == 0){
-                stop("Goseq analysis has not been performed.")
+                    stop("Goseq analysis has not been performed.")
                 }
             }
             name.eid <- names(module_output$modules)[n_plot]
@@ -123,7 +123,7 @@ setMethod(
             network <- module_output$network
             stat <- extractScores(pvalue_annotation)
             pval <- exp(stat/(-2))
-
+            
             if(class(network) == "graphNEL"){
                 network <- igraph::graph_from_graphnel(network)
                 adj_mat_network <- igraph::as_adjacency_matrix(network)
@@ -140,7 +140,7 @@ setMethod(
                 igraph::V(Graph_adj_mat)$weight <- stat.v
                 network <- Graph_adj_mat
             }
-
+            
             vect2color <- function(v, palettev, breaks) {
                 w <- v;
                 for (i in 1:length(palettev)) {
@@ -148,7 +148,7 @@ setMethod(
                 }
                 return(w);
             }
-
+            
             ## Vertex palette:
             vertexPalette.v <-
                 colorRampPalette(c("white","gray85","gray65","salmon"))(50)
@@ -160,13 +160,13 @@ setMethod(
                 colorRampPalette(c("white","gray85","gray65","salmon"))(50);
             edgeBreaks.v <- Hmisc::cut2(igraph::E(network)$weight, g=50,
                                         onlycuts=TRUE)
-
+            
             ## Compute iGraph object
             h <- igraph::induced_subgraph(network, eid);
             stat.v <- stat[igraph::V(h)$name];
             pval.v <- pval[igraph::V(h)$name];
             pval.v[which(pval.v == 0)] <- 0.000000001
-
+            
             par(mar=c(4, 0, 2, 0))
             ## Color edges between grey and red according to significance
             igraph::E(h)$color <- vect2color(igraph::E(h)$weight, edgePalette.v, edgeBreaks.v);
@@ -177,14 +177,15 @@ setMethod(
             vl <- igraph::V(h)$name
             igraph::V(h)$color[which(1-pchisq(igraph::V(h)$weight,2) < p_thresh)] <- "red"
             igraph::E(h)$color[which(1-pchisq(igraph::E(h)$weight,4) < p_thresh)] <- "red"
-
+            
             if(layout == "circle"){layout1 <- igraph::layout_in_circle(h)}
             if(layout == "fr"){layout1 <- igraph::layout_with_fr(h)}
+            if(layout == "dh"){layout1 <- igraph::layout_with_dh(h)}
+            if(layout == "kk"){layout1 <- igraph::layout_with_kk(h)}
             
-
             layout1_scaled <- cbind(scales::rescale(layout1[, 1], to=c(-1, 1)),
                                     scales::rescale(layout1[, 2], to=c(-1, 1)))
-
+            
             counter <- 0
             while(counter < 2){
                 plot(h, layout=layout1, ## same layout each time
@@ -194,17 +195,17 @@ setMethod(
                      vertex.label.font=3, vertex.label.color="black",
                      vertex.size =
                          if(length(igraph::V(h)) < 50) { 15 }
-                         else {15*13/length(igraph::V(h))},
+                     else {15*13/length(igraph::V(h))},
                      edge.width = if(length(igraph::V(h))< 50) { 2 } else { 1 },
                      ylim=c(-1, 1.5), xlim=c(-1, 1)
                 )
-
+                
                 halfCircle <- function(x, y, r, r2=.75, quarter=FALSE, start=0,
                                        end=pi, nsteps=30, col=NULL, lwd=1,
                                        border=NULL){
                     if(isTRUE(quarter))
                     {
-
+                        
                         rs <- seq(start, end, len=nsteps)
                         xc <- x+r*cos(rs)
                         yc <- y+r*sin(rs)
@@ -212,8 +213,8 @@ setMethod(
                         yc2 <- y+r*r2*sin(rs)
                         polygon(c(xc, rev(xc2)), c(yc, rev(yc2)), col=col,
                                 lwd=lwd, border=border)
-
-
+                        
+                        
                     }
                     if(!isTRUE(quarter))
                     {
@@ -223,77 +224,77 @@ setMethod(
                         polygon(xc, yc, col=col, lwd=lwd, border=border)
                     }
                 }
-
+                
                 arctext <- function(x, y, r, start, end, what, cex=1){
                     delta=(end-start)/4
                     rs <- seq(start, end, len=3)
                     xc <- x+r*cos(rs[2])
                     yc <- y+r*sin(rs[2])
                     text(xc, yc, what, srt=180+atan2(((y+r*sin(rs[3]))-
-                        (y+r*sin(rs[1]))),((x+r*cos(rs[3]))-(x+r*cos(rs[1]))
-                                           ))*180/pi, cex=cex )
+                                                          (y+r*sin(rs[1]))),((x+r*cos(rs[3]))-(x+r*cos(rs[1]))
+                                                          ))*180/pi, cex=cex )
                 }
-
+                
                 methcol <- c(meth_low_col, meth_mid_col, meth_hi_col, "white")
                 names(methcol) <- c("Low", "Med", "High", "NoData")
                 expcol <- c(exp_low_col, exp_mid_col, exp_hi_col, "white")
                 names(expcol) <- c("Low", "Med", "High", "NoData")
-
+                
                 pval_data <- slot(slot(pvalue_annotation, "score_data"), "pval_data")
                 genes_score <- slot(pvalue_annotation, "score_data")@genes
                 effect_data <- slot(slot(pvalue_annotation, "score_data"), "effect_data")
                 signs_idx <- slot(slot(pvalue_annotation, "score_data"), "signs_index")
                 if(any(suppress_details == FALSE, counter == 1)){
                     for(i in 1:nrow(layout1_scaled)){
-
+                        
                         halfCircle(x=layout1_scaled[i, 1], y=layout1_scaled[i, 2],
-                            r=ifelse(length(igraph::V(h)) < 50, 0.075, 0.025), start=pi/2,
-                            end=2*pi/2, quarter=TRUE, lwd=1,
-                            col=ifelse(!is.na(pval_data$expression_pvalue[which(
-                                genes_score %in% igraph::V(h)$name[i])]),
-                                expcol[ifelse(abs(pval_data$expression_pvalue[
-                                    which(genes_score %in% igraph::V(h)$name[i])
-                                    ]) < p_thresh, ifelse(sign(
-                                        effect_data$expression_effect[which(
-                                            genes_score %in%
-                                                igraph::V(h)$name[i])]) ==
-                                            1, 3, 1), 2)], expcol[4])
+                                   r=ifelse(length(igraph::V(h)) < 50, 0.075, 0.025), start=pi/2,
+                                   end=2*pi/2, quarter=TRUE, lwd=1,
+                                   col=ifelse(!is.na(pval_data$expression_pvalue[which(
+                                       genes_score %in% igraph::V(h)$name[i])]),
+                                       expcol[ifelse(abs(pval_data$expression_pvalue[
+                                           which(genes_score %in% igraph::V(h)$name[i])
+                                           ]) < p_thresh, ifelse(sign(
+                                               effect_data$expression_effect[which(
+                                                   genes_score %in%
+                                                       igraph::V(h)$name[i])]) ==
+                                                   1, 3, 1), 2)], expcol[4])
                         )
-
+                        
                         start <- pi
                         delta <- (3*pi/2)/nrow(signs_idx)
-
+                        
                         for(j in signs_idx[, 3]){
-
+                            
                             score_graph_col<-returnPvalueCol(slot(
                                 pvalue_annotation, "score_data"),
                                 j)[which(genes_score %in% igraph::V(h)$name[i])]
-
+                            
                             halfCircle(x=layout1_scaled[i, 1],
-                                y=layout1_scaled[i, 2],
-                                r=ifelse(length(igraph::V(h)) <
-                                             50, 0.075, 0.025),
-                                start=start, end=start+delta, quarter=TRUE,
-                                col=ifelse(!is.na(score_graph_col),
-                                    methcol[ifelse(abs(score_graph_col) <
-                                                       p_thresh,
-                                        ifelse(effect_data[, grep(j, colnames(
-                                        effect_data))][which(genes_score %in%
-                                        igraph::V(h)$name[i])] == 1,
-                                        3, 1), 2)],methcol[4])
-                                )
+                                       y=layout1_scaled[i, 2],
+                                       r=ifelse(length(igraph::V(h)) <
+                                                    50, 0.075, 0.025),
+                                       start=start, end=start+delta, quarter=TRUE,
+                                       col=ifelse(!is.na(score_graph_col),
+                                                  methcol[ifelse(abs(score_graph_col) <
+                                                                     p_thresh,
+                                                                 ifelse(effect_data[, grep(j, colnames(
+                                                                     effect_data))][which(genes_score %in%
+                                                                                              igraph::V(h)$name[i])] == 1,
+                                                                     3, 1), 2)],methcol[4])
+                            )
                             start <- start+delta
                         }
                     }
                 }
-
+                
                 if(any(legend == TRUE, counter == 1)){
                     if(any(suppress_details == FALSE, counter == 1)){
                         num_factors <- nrow(signs_idx)
                         halfCircle(x=-1.25, y=1.25, r=.4, start=pi/2, end=pi, quarter=TRUE)
                         start <- pi/2
                         delta <- pi/8
-
+                        
                         for(g in 1:4){
                             halfCircle(x=-1.25, y=1.25, r=.37, r2=.89, start=start+delta*(g-1),
                                        end=start+delta*g, col=expcol[g], quarter=TRUE)
@@ -303,14 +304,14 @@ setMethod(
                         text(-1.25+1.2*cos(seq(start, start+delta*2, len=30)[15]),
                              1.25+.4*.75*sin(seq(start,start+delta*2, len=30)[15]),
                              "expression", cex=.6)
-
+                        
                         start <- pi
                         delta <- (3*pi/2)/num_factors
-
+                        
                         for(j in signs_idx[, 3]){
                             halfCircle(x=-1.25, y=1.25, r=.4, start=start,
                                        end=start+delta, quarter=TRUE)
-
+                            
                             for(g in 1:4){
                                 start <- start
                                 end <- start+delta
@@ -319,17 +320,17 @@ setMethod(
                                            start=start+delta2*(g-1),
                                            end=start+delta2*g,
                                            col=methcol[g], quarter=TRUE)
-
+                                
                                 arctext(x=-1.25, y=1.25, r=.35,
                                         start=start+delta2*(g-1),
                                         end=start+delta2*g,
                                         ifelse(num_factors<=4,
                                                names(methcol)[g],
-                                                       substring(names(methcol)[g],
-                                                                 1, 1)),
+                                               substring(names(methcol)[g],
+                                                         1, 1)),
                                         cex=.5)
                             }
-
+                            
                             text(-1.25+.57*cos(seq(start, start+delta, len=30)[15]),
                                  1.25+.57*.75*sin(seq(start, start+delta, len=30)[15]),
                                  paste(strsplit(j, "_")[[1]], collapse="\n"), cex=.6)
@@ -337,22 +338,22 @@ setMethod(
                         }
                     }
                     halfCircle(x=-1.25, y=1.25, r=.3, end=2*pi, col="white")
-
+                    
                     vertexPalette.v[which(
                         vertexBreaks.v >= min(igraph::V(h)$weight[which(
                             igraph::V(h)$weight >= qchisq(1-p_thresh,2))])) - 1] <- "red"
-
+                    
                     points(seq(-1.45, -1.05, length.out=50), rep(1.35, 50),
                            col=vertexPalette.v, pch=15, cex=2.5)
-
+                    
                     text(-1.20, 1.44, expression('node             ',Chi[2]^2))
                     text(-1.45, 1.295, round(vertexBreaks.v[1], 2))
                     text(-1.25, 1.295, round(vertexBreaks.v[26], 2))
                     text(-1.07, 1.295, paste(">",round(qchisq(1-p_thresh,2), 2),sep=""))
-
+                    
                     if(length(which(
                         igraph::E(h)$weight >= qchisq(1-p_thresh, 4))) > 0){
-
+                        
                         edgePalette.v[which(
                             edgeBreaks.v >= min(igraph::E(h)$weight[which(
                                 igraph::E(h)$weight >= qchisq(1-p_thresh, 4))]))-1] <- "red"
@@ -364,62 +365,62 @@ setMethod(
                     text(-1.25, 1.095, round(edgeBreaks.v[26], 2))
                     text(-1.07, 1.095, paste(">", round(qchisq(1-p_thresh,4), 2), sep=""))
                 }
-
+                
                 addShadowText(layout1_scaled[, 1], layout1_scaled[, 2], vl, font=2,
                               cex=if(label_scale == TRUE){
                                   scales::rescale(stat.v, to=(c(.5, 2)))
                               }
                               else{.5},
                               bg="white", col="black")
-
+                
                 if(namestyle == "refseq"){
                     ref2eg <- AnnotationDbi::as.list(org.Hs.eg.db::org.Hs.egREFSEQ2EG)
                     eg2sym <- AnnotationDbi::as.list(org.Hs.eg.db::org.Hs.egSYMBOL)
-
+                    
                     text(layout1_scaled[, 1], layout1_scaled[, 2]-.05,
                          sapply(vl,
                                 function(k){
                                     ifelse(is.null(ref2eg[[k]]), return(NA),
-                                    return(eg2sym[[ref2eg[[k]]]]))
+                                           return(eg2sym[[ref2eg[[k]]]]))
                                 })
-                         )
-
+                    )
+                    
                     text(0, 1.65, paste("Network built around", name.eid,
-                            ifelse(is.null(ref2eg[[name.eid]]), NA,
-                            eg2sym[[ref2eg[[name.eid]]]])))
+                                        ifelse(is.null(ref2eg[[name.eid]]), NA,
+                                               eg2sym[[ref2eg[[name.eid]]]])))
                 }
                 if(namestyle == "symbol"){
                     text(0, 1.7, paste(
                         "Network built around", name.eid, "\nChi-square P-value=",
                         round(module_output$moduleStats[[n_plot]][2],4)))
                 }
-
+                
                 if(goseq == TRUE){
                     if(nrow(module_output$goseqOut[[n_plot]]) > 0){
-
+                        
                         text("Num\nGenes", x=1.35, y=1.6, font=2)
                         text("Enriched\nPathway/Term", x=2, y=1.6, font=2)
                         for(i in 1:nrow(module_output$goseqOut[[n_plot]])){
                             text(module_output$goseqOut[[n_plot]][i, 4], x=1.3,
                                  y=seq(1.4, -1, length.out=nrow(module_output$goseqOut[[n_plot]]))[i],
                                  adj = c(0, 0))
-
+                            
                             text(module_output$goseqOut[[n_plot]][i, 6], x=1.5,
                                  y=seq(1.4, -1, length.out=nrow(module_output$goseqOut[[n_plot]]))[i],
                                  adj= c(0, 0))
                         }
-
+                        
                     }
                     else {
                         text("No enriched terms from\nGoseq", x=1.5, y=1.6, font=2)
-
+                        
                     }
                 }
-
+                
                 if(counter == 1){break}
                 counter <- 2
             }
-
+            
             if(is.null(pdf_out)){
                 if(n_plot != which_network[length(which_network)]){
                     message("Press key to go to next plot")
